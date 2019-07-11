7b900a9c39173d9535ba54af3dbcc654510efa27
swastiktiwari1 <36890412+swastiktiwari1@users.noreply.github.com>
2019-07-12 02:04:05 +0530

    Create Sex_prediction_based_on_heights.r

---
+++ Sex_prediction_based_on_heights.r
@@ -0,0 +1,25 @@
+library('dslabs')
+data(heights)
+y<-heights$sex
+x<-heights$height
+set.seed(2)
+test_index<-createDataPartition(y,times=1,p=0.5,list=FALSE)
+train_set<-heights[-test_index,]
+test_set<-heights[test_index,]
+x<-c("Male","Female")
+y_hat<-sample(x,size=length(test_index),replace=TRUE)%>%factor(levels = levels(test_set$sex))
+mean(y_hat==test_set$sex)
+y_hat<-ifelse(x>62,"Male","Female")%>%factor(levels=levels(test_set$sex))
+mean(y==y_hat)
+cutoff <- seq(61, 70)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  mean(y_hat == train_set$sex)
+})
+max(accuracy)
+best_cutoff<-cutoff[which.max(accuracy)]
+best_cutoff
+y_hat<-ifelse(test_set$height>best_cutoff,"Male","Female")%>%factor(levels=levels(test_set$sex))
+y_hat<-factor(y_hat)
+mean(y_hat==test_set$sex)
